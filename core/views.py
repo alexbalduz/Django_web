235bc22b167dff9808ef701bc0c4305539675b14
alexbalduz <83299400+alexbalduz@users.noreply.github.com>
2022-11-15 17:40:45 +0100

    funciona todo :)

--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse
+
 # Create your views here.
 
 def home(request):
@@ -6,6 +7,7 @@ def home(request):
 
 def about(request):
     return render(request, "core/about.html")
+
 
 def portfolio(request):
     return render(request, "core/portfolio.html")
